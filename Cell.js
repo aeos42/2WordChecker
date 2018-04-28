5fc5f6829e9b61d70c1ff65a492a4f36f66733ff
aeos42 <aeos42@gmail.com>
2018-04-28 17:38:45 -0600

    initial letter displayed

--- Cell.js
+++ Cell.js
@@ -1,4 +1,32 @@
 // Cell.js
+
+class Cell {
+  constructor(row, col) {
+    this.letter = "9";
+    this.resolution = RESOLUTION;
+    this.color = color(255);
+    this.row = row;
+    this.col = col;
+    }
+    
+    displayCell(cell) {
+      noStroke();
+      fill(this.color);
+      rect(this.row * RESOLUTION, this.col * RESOLUTION, RESOLUTION, RESOLUTION);
+      fill(0);
+      textAlign(CENTER);
+      text(this.letter, this.row * RESOLUTION, this.col * RESOLUTION);
+      console.log(cell);
+    }
+    
+  }
+
+
+
+
+
+
+/*
 function Cell(_x, _y, _resolution, _index) {
   this.x = _x;
   this.y = _y;
@@ -21,3 +49,4 @@ function Cell(_x, _y, _resolution, _index) {
     text(this.letter, this.x * this.resolution + 6, this.y * this.resolution + 10);
   }
 }
+*/
--- sketch.js
+++ sketch.js
@@ -1,38 +1,78 @@
 // 2 Word Checker
-var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+const LETTERS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+const HEIGHT = 600;
+const WIDTH = 600;
+const RESOLUTION = 24;
+
+const ROWS = HEIGHT / RESOLUTION;
+const COLS = WIDTH / RESOLUTION;
+
+var cellGrid;
+var cellQueue = [];
+var drawCells = [];
+
+//makes a 2d array of cell objects
+function makeGrid(_rows, _cols) {
+	var grid = [];
+
+	for (var i = 0; i < _rows; i++) {
+	  grid[i] = [];
+	  for (var j = 0; j < _cols; j++) {
+		  grid[i].push(new Cell());
+	  }
+  }
+  console.log(grid);
+	return grid;
+}
+
+function randomLetter(_letters) {
+  return _letters[floor(random(_letters.length))]
+}
+
+
+function initLetter() {
+  var row = floor(random(cellGrid.length));
+  var col = floor(random(cellGrid[0].length));
+
+  var firstCell = cellGrid[row][col];
+
+  firstCell.row = row;
+  firstCell.col = col;
+  firstCell.letter = randomLetter(LETTERS);
+
+  return firstCell;
+}
 
 function preload() {
   words = loadStrings("twowords.txt");
-}
-
-var cells = [];
-let cols;
-let rows;
-let resolution = 12;
-var myCell;
-
-function setup() {
-  createCanvas(600, 600);
-  cols = width / resolution;
-  rows = height / resolution;
+  
   for (var i = 0; i < words.length; i++) {
     words[i] = words[i].toLowerCase();
   }
-  noLoop();
-  cells.push(new Cell(0, 0, resolution, 0));
+}
+
+function setup() {
+  createCanvas(600, 600);
+
+  cellGrid = makeGrid(ROWS, COLS);
   textSize(10);
   textFont('Georgia');
+  //draws one frame a second
+  frameRate(1);
+
+  var initialCell = initLetter();
+  initialCell.displayCell();
 }
 
 function draw() {
-  background(0);
-
-  for (let i = 0; i < cells.length; i++) {
-    cells[i].display();
-    if (i < 300) {
-      createNewCell(cells[i]);
-    }
+  //background(0);
+  //addLetters();
+  /*
+  for (let i = 0; i < drawCells.length; i++) {
+    drawCells[i].displayCell();
   }
+  */
+
 }
 
 function createNewCell(c) {
